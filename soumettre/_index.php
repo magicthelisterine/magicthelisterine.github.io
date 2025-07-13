5938b330d428044c8f1b6eb3a771a8a88cf1973e
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-07-13 02:10:38 -0400

    Late night changements

---
+++ soumettre/_index.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @index    8
+ * @type     article
+ * @title    Soumettre une carte
+ * @icon     images/icon.webp
+ * @image    ../assets/images/og-image.webp
+ * @abstract Formulaire de soumission de carte
+ */
+?>
+
