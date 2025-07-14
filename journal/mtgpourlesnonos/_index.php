3b621bbe432bbeeb020f7bb194dbe53f239fc86e
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-07-14 07:56:49 -0400

    Ajout de la page MTG pour les nonos

---
+++ journal/mtgpourlesnonos/_index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * @type     article
+ * @title    MTG pour les nonos
+ * @icon     images/icon.webp
+ * @image    ../images/image.webp
+ * @abstract Guide pour créer une carte Magic comme si t'étais nono
+ */
+?>
+
+<gdoc id="AKfycbwyRGePI7udCN3v98RvwdDyAxk3L_SpHgGFuAJdnmQhb-Y3J18zME2fdkg9VbUiKUNBGQ"></gdoc>
+
+<dots></dots>
+
+<doclink href="https://docs.google.com/document/d/1YeJOUrEQ4B_qwt-EjPBopSrg2Ny3QI5OTE6oUz2OM88/edit">Modifier le document</doclink>
+
+<p><small>Dernière mise-à-jour: <em><compdate></compdate></em></small></p>
